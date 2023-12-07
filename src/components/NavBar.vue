996a1806e0173434f885f95bf010c718b51e89d2
SantiagoG1993 <gamarras1993@gmail.com>
2023-12-07 00:07:15 -0300

    agregado componente navbar y presentation

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar">
+        <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        <h4 id="name">Santiago Gamarra</h4>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.navbar{
+    width: 100%;
+    height: 80px;
+    user-select: none;
+    background-color: rgb(238, 238, 238);
+    display: flex;
+    align-items: center;
+    position: relative;
+}
+ul{
+    display: flex;
+    gap: 20px;
+    list-style-type: none;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 25px;
+    color: rgb(78, 77, 77);
+}
+ul li:hover{
+color: rgb(146, 146, 146);
+cursor: pointer;
+}
+#name{
+    font-size: 40px;
+    font-family: Arial, Helvetica, sans-serif;
+    position: absolute;
+    right:30px;
+}
+</style>
---
+++ src/components/PresentationProfile.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container_presentation">
+        <div class="photo_text_c">
+    <img src="" alt=""> 
+    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit animi enim, aperiam aliquid consequatur necessitatibus porro repellat eos qui voluptatibus repellendus a vel pariatur itaque. Beatae, cumque assumenda. Tempora, dolores!</p>
+        </div>
+    <div class="btns_c">
+        <button>Hire me</button>
+        <button>Download CV</button>
+    </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.container_presentation{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 450px;
+    background-color: rgb(63, 63, 63);
+    justify-content: space-around;
+}
+img{
+    background-color: white;
+    width: 320px;
+    height: 320px;
+    border-radius: 50%;
+    margin-left: 30px;
+}
+.text{
+    width: 600px;
+    height: 300px;
+    font-size: 28px;
+    color: white;
+    display: flex;
+    align-items: center;
+}
+.photo_text_c{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 65%;
+    justify-content: space-between;
+    }
+button{
+    width: 150px;
+    height: 50px;
+    border-radius: 19px;
+    border: none;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.1em;
+    cursor: pointer;
+}
+button:hover{
+    background-color: transparent;
+    border: 2px solid white;
+    color: white;
+    transition: .3s all ease-in;
+}
+.btns_c{
+    display: flex;
+    gap: 20px;
+}
+</style>
