068e3625f7224cdff16da5899062c92a5f017f65
SantiagoG1993 <gamarras1993@gmail.com>
2023-12-11 22:41:14 -0300

    agregue parte contact

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <PresentationProfile />
     <AboutMe />
     <ProjectsComponent />
-    <SocialMedia />
     <ContactMe />
   </div>
 </template>
@@ -15,7 +14,6 @@ import PresentationProfile from './components/PresentationProfile.vue'
 import AboutMe from './components/AboutMe.vue'
 import ProjectsComponent from './components/ProjectsComponent.vue'
 import ContactMe from './components/ContactMe.vue'
-import SocialMedia from './components/SocialMedia.vue'
 </script>
 
 <style scoped>
--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="contactme_c">
+    <div class="contactme_c">   
+        <div class="contact_form">
+            <h1>CONTACT ME</h1>
         <form action="">
             <input type="email" placeholder="nombre@email.com" >
             <input type="text" id="subject" placeholder="Subject">
             <input type="text" id="message" placeholder="message">
             <button>Send</button>
         </form>
+        </div>
+        <div class="social_c">
+            <h2>SOCIAL</h2>
+                <i class="fa-brands fa-github"></i>
+                <i class="fa-brands fa-linkedin"></i>
+        </div>
 
     </div>
 </template>
@@ -15,13 +23,49 @@
 </script>
 
 <style scoped>
+*{
+    margin: 0px!important;
+    padding: 0px!important;
+}
 .contactme_c{
 width: 100%;
 height: 400px;
-background-color: rgb(34, 34, 34);
+display: flex;
+gap: 20px;
+justify-content: space-around;
+margin-top: 33px!important;
+}
+h1,h2{
+    color: white;
+    font-family: 'Jaldi', sans-serif;
+    align-self: flex-start;
+    margin-left: 50px!important;
+}
+.contact_form{
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid white;
+    border-radius: 45px 0px 0px 0px;
+    background-color: rgb(34, 34, 34);
+}
+.social_c{
+    width: 40%;
+    height: 100%;
+    background-color: rgb(34, 34, 34);
+}
+.fa-github, .fa-linkedin{
+    font-size: 80px;
+    color: white;
+}
+h2{
+    font-size: 30px;
 }
 form{
-    width: 700px;
+    width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
@@ -36,11 +80,12 @@ input, button{
     width: 90%;
     height: 40px;
     border: none;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: 'Inter', sans-serif;
+    font-style: italic;
     cursor: pointer;
 }
 input{
-    padding-left: 15px;
+    padding-left: 15px!important;
 }
 input:focus{
     outline: none;
@@ -51,7 +96,7 @@ input:focus{
 }
 button{
     width: 93%;
-   background-color: rgb(4, 97, 140);
+   background-color: #343635;
     color: white;
 }
 button:hover{
